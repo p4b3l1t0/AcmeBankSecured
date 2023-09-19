2d49695385fcae3c251ff11548e59e8182d330e9
p4b3l1t0 <pablosalinas1717@gmail.com>
2023-09-19 04:19:31 +0100

    adding new security protection features to the node app v1.0 - helmet - CookieParser - Validator - Csurf - SQL, PT, XSS sanitization, etc.

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const fs = require("fs");
+const { request } = require("http");
+// adding Helmet to the Node App
+const helmet = require("helmet");
+// adding Validator to the Node App
+const validator = require("express-validator");
+// adding Cookie Parser to the Node App
+const cookieparser = require("cookie-parser");
+// adding CSRF to the Node App
+const csurf = require("csurf");
+// adding Rate Limit protection
+//const rateLimit = require("express-rate-limit").default;
+// adding Environment variables secure management package
+//const dotenv = require("dotenv");
 
 const db = new sqlite3.Database("./bank_sample.db");
 
@@ -10,12 +23,51 @@ const app = express();
 const PORT = 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+// use helmet
+app.use(helmet());
+// use CookieParser
+app.use(cookieparser());
+// Apply rate limiting to all routes
+//app.use(limiter);
+
+// Set up rate limiting using the express-rate-limit middleware
+//const limiter = rateLimit({
+//  windowMs: 15 * 60 * 1000, // 15 minutes
+ // max: 100, // limit each IP to 100 requests per windowMs
+  //message: 'Too many requests from this IP, please try again later'
+//});
+
+// Apply rate limiting to all routes
+//app.use(limiter);
+
+// Setting up CSRF protection
+const csrfMiddleware = csurf({
+  cookie: {
+    sameSite: "none", 
+  },
+});
+
+// Create a Middleware to add CSRF protection to specific endpoints
+
+app.use((error, request, response, next) => {
+  if (error.code === "EBADCSRFTOKEN") {
+    response.status(403);
+    response.send("The CSRF token is invalid");
+  } else {
+    next();
+  }
+});
 
 app.use(
   session({
     secret: "secret",
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      maxAge: 30000,
+      secure: true,
+      httpOnly: true,
+    },
   })
 );
 
@@ -26,13 +78,14 @@ app.get("/", function (request, response) {
   response.sendFile(path.join(__dirname + "/html/login.html"));
 });
 
-//LOGIN SQL
+//LOGIN SQL FIXED
 app.post("/auth", function (request, response) {
   var username = request.body.username;
   var password = request.body.password;
   if (username && password) {
     db.get(
-      `SELECT * FROM users WHERE username = '${request.body.username}' AND password = '${request.body.password}'`,
+      `SELECT * FROM users WHERE username = ? AND password = ?`,
+      [username, password],
       function (error, results) {
         console.log(error);
         console.log(results);
@@ -68,17 +121,17 @@ app.get("/home", function (request, response) {
 });
 
 //CSRF CODE SECURED. SEE HEADERS SET ABOVE
-app.get("/transfer", function (request, response) {
+app.get("/transfer", csrfMiddleware, function (request, response) {
   if (request.session.loggedin) {
     var sent = "";
-    response.render("transfer", { sent });
+    response.render("transfer", { sent, csrfToken: request.csrfToken() });
   } else {
     response.redirect("/");
   }
 });
 
 //CSRF CODE
-app.post("/transfer", function (request, response) {
+app.post("/transfer", csrfMiddleware, function (request, response) {
   if (request.session.loggedin) {
     console.log("Transfer in progress");
     var balance = request.session.balance;
@@ -114,7 +167,7 @@ app.post("/transfer", function (request, response) {
   }
 });
 
-//PATH TRAVERSAL CODE
+//PATH TRAVERSAL CODE FIXED
 app.get("/download", function (request, response) {
   if (request.session.loggedin) {
     file_name = request.session.file_history;
@@ -129,15 +182,27 @@ app.post("/download", function (request, response) {
   if (request.session.loggedin) {
     var file_name = request.body.file;
 
+    // const filePath = "history_files/" + file_name; ----> Vulnerable way to manipulate files in Node Apps
+
+    // Safe way to manipulated files in Node Apps
+    const rootDirectory = "history_files\\";
+    const filePath = path.join(process.cwd() + '/history_files/', file_name);
+    const fileName = path.normalize(filePath);
+      
+
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
+    console.log(filePath);
 
     // Change the filePath to current working directory using the "path" method
-    const filePath = "history_files/" + file_name;
-    console.log(filePath);
+  
     try {
-      content = fs.readFileSync(filePath, "utf8");
-      response.end(content);
+      if (fileName.indexOf(rootDirectory) < 0) {
+        response.send("File not found!");
+      } else {
+        content = fs.readFileSync(filePath, "utf8");
+        response.end(content);
+      }
     } catch (err) {
       console.log(err);
       response.end("File not found");
@@ -148,7 +213,7 @@ app.post("/download", function (request, response) {
   response.end();
 });
 
-//XSS CODE
+//XSS CODE FIXED
 app.get("/public_forum", function (request, response) {
   if (request.session.loggedin) {
     db.all(`SELECT username,message FROM public_forum`, (err, rows) => {
@@ -164,11 +229,12 @@ app.get("/public_forum", function (request, response) {
 
 app.post("/public_forum", function (request, response) {
   if (request.session.loggedin) {
-    var comment = request.body.comment;
+    var comment = validator.escape(request.body.comment);
     var username = request.session.username;
     if (comment) {
       db.all(
-        `INSERT INTO public_forum (username,message) VALUES ('${username}','${comment}')`,
+        `INSERT INTO public_forum (username,message) VALUES (?, ?)`,
+        [username, message],
         (err, rows) => {
           console.log(err);
         }
@@ -193,15 +259,16 @@ app.post("/public_forum", function (request, response) {
   //response.end();
 });
 
-//SQL UNION INJECTION
+//SQL UNION INJECTION FIXED
 app.get("/public_ledger", function (request, response) {
   if (request.session.loggedin) {
-    var id = request.query.id;
+    var id = parseInt(request.query.id);
     if (id) {
       db.all(
-        `SELECT * FROM public_ledger WHERE from_account = '${id}'`,
+        `SELECT from_account,to_account,amount FROM public_ledger WHERE from_account = ?`,
+        [id],
         (err, rows) => {
-          console.log("PROCESSING INPU");
+          console.log("PROCESSING INPUT");
           console.log(err);
           if (rows) {
             response.render("ledger", { rows });
@@ -211,13 +278,16 @@ app.get("/public_ledger", function (request, response) {
         }
       );
     } else {
-      db.all(`SELECT * FROM public_ledger`, (err, rows) => {
-        if (rows) {
-          response.render("ledger", { rows });
-        } else {
-          response.render("ledger", { rows });
+      db.all(
+        `SELECT from_account,to_account,amount FROM public_ledger`,
+        (err, rows) => {
+          if (rows) {
+            response.render("ledger", { rows });
+          } else {
+            response.render("ledger", { rows });
+          }
         }
-      });
+      );
     }
   } else {
     response.redirect("/");
